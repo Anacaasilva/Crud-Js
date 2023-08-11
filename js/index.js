c588b3b8d9919f30f00cf0b7da9aff3933c6cb5e
Ana Carolina <anacarolina.oliveira1020@gmail.com>
2023-08-11 15:48:28 -0300

    finalizing edit and delete actions

--- js/index.js
+++ js/index.js
@@ -3,23 +3,59 @@ const inputPrice = document.getElementById('price');
 const buttonSubmit = document.getElementById('submit');
 const tbody = document.getElementsByTagName('tbody')[0]; //pegar o indice 0 da lista que o getElement retorna
 
-let list = [{nome:'arroz', preco:'12,00'}];
+let list = [];
+let editando = false;
+let posicao = -1; //posição do item no array quando editado 
 
 const editIten = (index) => {
 
-  inputName.value = list[index].nome; // atribuindo value do input identificando o item na lista atraves do index
-  inputPrice.value =list[index].preco;
+  let itenSelecionado = list[index];
 
-  console.log(list[index]);
-  console.log(list[index].nome);
+  inputName.value = itenSelecionado.nome; // atribuindo value do input identificando o item na lista atraves do index
+  inputPrice.value = itenSelecionado.preco;
+  
+  editando = true;
+
+  buttonLabel();
+
+  posicao = index;
+
+}
+
+const buttonLabel = () => {
+
+  buttonSubmit.innerHTML = `${editando? 'SALVAR': 'CADASTRAR'}`;
+
+}
+
+
+const salvar = () => {
+
+  tbody.innerHTML = ``; //limpando a tabela antes de adicionar um novo item 
+
+  let newItem = {
+    nome: inputName.value,
+    preco: inputPrice.value,
+  }; //criando o objeto com as propriedades do novo item
+
+  list[posicao] = newItem;
+
+  inputName.value = '';
+  inputPrice.value = '';
+
+  showCards();
+
+  editando = false;
+
+  buttonLabel();
 
 }
 
 const deleteIten = (index) => {
 
-  list.splice(index, 1)
-  tbody.innerHTML = ``;
-  showCards();
+  list.splice(index, 1); // apaga 1 item a partir do index passado por parametro
+  tbody.innerHTML = ``; // limpo minha tabela 
+  showCards(); // renderizo os itens novamente.
 
 }
 
@@ -67,6 +103,7 @@ const validate = () => {
 
   if (inputName.value == '') alert('Campo "Nome" deve ser preenchido!!');
   else if (inputPrice.value == '') alert('Campo "Preço" deve ser preenchido!!');
+  else if (editando) salvar();
   else createItem();
 
 }
